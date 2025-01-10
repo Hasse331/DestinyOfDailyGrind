9eb3b2c1229d0ce8d8a850c42592ef2e65e99a79
Hasse <imberg.hasse@pm.me>
2025-01-10 13:18:54 +0200

    setup commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("------ Destiny of Daily Grind -----");
+        System.out.println("## The best Java CLI game in the world ##");
+        System.out.println("## Winner of the game of the year awards 2226 ##");
+        System.out.println("\n\nPress ENTER to start a new game...");
+    
+        Scanner scanner = new Scanner(System.in);
+        scanner.nextLine();
+        startNewGame();
+    
+    }
+
+    public static void startNewGame() {
+        
+    }
+}
